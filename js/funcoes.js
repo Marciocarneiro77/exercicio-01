f8b3e340544d8378c489c292a94d96c7fbb2a538
Marcio Carneiro 77 <mac.carneiro@yahoo.com.br>
2020-12-08 17:42:24 -0300

    exercicios de java script

---
+++ js/funcoes.js
@@ -0,0 +1,20 @@
+console.log("Olá Marcio Carneiro");
+let nome = prompt("Informe seu nome")
+alert("Olá" + nome + "Seja bem vindo!");
+
+let numero1 = prompt("Digite um número");
+let numero2 = prompt("Digite outro número");
+
+//se(numero > 20)
+if(numero1>20){
+    //escreva "numero é maior que 20"
+    alert("Número é maior que 20");
+
+}
+
+else{
+    //escreva "número é menor que 20"
+    alert("Número é menor que20");
+
+}
+//fim se
